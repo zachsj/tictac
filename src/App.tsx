26fcbae77a21e6836f0b157e5192b4306a372114
Zach Johnson <yorkmacbook026@Yorks-MacBook-026.local>
2025-06-10 17:46:46 -0400

    finished tic-toc

--- src/App.tsx
+++ src/App.tsx
@@ -77,14 +77,35 @@ function Board({ xIsNext, squares, onPlay }: BoardProps) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: (string | null)[]) {
-    setHistory([...history, nextSquares]); //creates a new array that contains all the items in history, followed by nextSquares
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+   function jumpTo(nextMove: number): void {
+    setCurrentMove(nextMove);
+  }
+
+  //the squares argument goes through each element of history, and the move argument goes through each array index: 0, 1, 2
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -92,7 +113,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
